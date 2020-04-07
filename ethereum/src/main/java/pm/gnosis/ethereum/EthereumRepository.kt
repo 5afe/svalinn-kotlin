951d765e667f08b872bb7cbcc088cd1745526c15
Jose Pablo Alvarez Lacasia <jose.alvarez@gnosis.io>
2020-04-07 18:06:14 +0200

    Cleaned up comments and redudant files

--- ethereum/src/main/java/pm/gnosis/ethereum/EthereumRepository.kt
+++ ethereum/src/main/java/pm/gnosis/ethereum/EthereumRepository.kt
@@ -1,37 +1,9 @@
 package pm.gnosis.ethereum
 
-import pm.gnosis.ethereum.models.EthereumBlock
-import pm.gnosis.ethereum.models.TransactionData
-import pm.gnosis.ethereum.models.TransactionParameters
-import pm.gnosis.ethereum.models.TransactionReceipt
 import pm.gnosis.model.Solidity
 import pm.gnosis.models.Transaction
 import pm.gnosis.models.Wei
 import java.math.BigInteger
-
-//interface EthereumRepository {
-//
-//    fun <R : BulkRequest> request(bulk: R): R
-//
-//    fun <R : EthRequest<*>> request(request: R): R
-//
-//    fun getBalance(address: Solidity.Address): Wei
-//
-//    fun sendRawTransaction(signedTransactionData: String): String
-//
-//    fun getTransactionReceipt(transactionHash: String): TransactionReceipt
-//
-//    fun getTransactionByHash(transactionHash: String): TransactionData
-//
-//    fun getBlockByHash(blockHash: String): EthereumBlock
-//
-//    fun getTransactionParameters(
-//        from: Solidity.Address,
-//        to: Solidity.Address,
-//        value: Wei? = null,
-//        data: String? = null
-//    ): TransactionParameters
-//}
 
 open class BulkRequest(val requests: List<EthRequest<*>>) {
     constructor(vararg requests: EthRequest<*>) : this(requests.toList())
