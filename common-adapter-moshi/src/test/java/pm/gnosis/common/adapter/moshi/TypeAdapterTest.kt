d3b94a99fa57bcacf250f9108f37a0306fed5c79
Dirk Jäckel <dirk.jaeckel@gnosis.pm>
2020-04-22 13:08:34 +0200

    Adding more tests

--- common-adapter-moshi/src/test/java/pm/gnosis/common/adapter/moshi/TypeAdapterTest.kt
+++ common-adapter-moshi/src/test/java/pm/gnosis/common/adapter/moshi/TypeAdapterTest.kt
@@ -2,6 +2,7 @@ package pm.gnosis.common.adapters.moshi
 
 import org.junit.Assert.assertEquals
 import org.junit.Test
+import pm.gnosis.model.Solidity
 import pm.gnosis.models.Wei
 import java.math.BigInteger
 
@@ -17,5 +18,53 @@ class TypeAdapterTest {
     fun testWeiAdapterToJson() {
         val weiString = WeiAdapter().toJson(Wei(BigInteger("1000000000000000000")))
         assertEquals("0xde0b6b3a7640000", weiString)
+    }
+
+    @Test
+    fun testHexNumberAdapterFromJson() {
+        val value = HexNumberAdapter().fromJson("0xde0b6b3a7640000")
+        assertEquals(BigInteger("1000000000000000000"), value)
+    }
+
+    @Test
+    fun testHexNumberAdapterToJson() {
+        val hexNumberString = HexNumberAdapter().toJson(BigInteger("1000000000000000000"))
+        assertEquals("0xde0b6b3a7640000", hexNumberString)
+    }
+
+    @Test
+    fun testDecimalNumberAdapterFromJson() {
+        val value = DecimalNumberAdapter().fromJson("1000000000000000000")
+        assertEquals(BigInteger("1000000000000000000"), value)
+    }
+
+    @Test
+    fun testDecimalNumberAdapterToJson() {
+        val decimalString = DecimalNumberAdapter().toJson(BigInteger("1000000000000000000"))
+        assertEquals("1000000000000000000", decimalString)
+    }
+
+    @Test
+    fun testDefaultNumberAdapterFromJson() {
+        val weiValue = WeiAdapter().fromJson("0xde0b6b3a7640000")
+        assertEquals(BigInteger("1000000000000000000"), weiValue.value)
+    }
+
+    @Test
+    fun testDefaultNumberAdapterToJson() {
+        val numberString = DefaultNumberAdapter().toJson(BigInteger("1000000000000000000"))
+        assertEquals("0xde0b6b3a7640000", numberString)
+    }
+
+    @Test
+    fun testSolidityAddressAdapterFromJson() {
+        val addressValue = SolidityAddressAdapter().fromJson("0x0000000000000000000000000de0b6B3a7640000")
+        assertEquals(BigInteger("1000000000000000000"), addressValue.value)
+    }
+
+    @Test
+    fun testSolidityAddressAdapterToJson() {
+        val addressString = SolidityAddressAdapter().toJson(Solidity.Address(BigInteger("1000000000000000000")))
+        assertEquals("0x0000000000000000000000000de0b6B3a7640000", addressString)
     }
 }
